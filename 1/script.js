fb5458789edd1d9dc407d9a99c1ec411f3815cd5
Kay Wild <kwild01@ch-c02z42m7lvdq.home>
2022-02-01 11:53:11 +0100

    finish part two

--- 1/script.js
+++ 1/script.js
@@ -6,6 +6,7 @@ const arr = fs
   .filter((x) => Boolean(x))
   .map((x) => parseInt(x));
 
+// Part 1
 let count = 0;
 
 for (let i = 1; i < arr.length; i++) {
@@ -15,3 +16,16 @@ for (let i = 1; i < arr.length; i++) {
 }
 
 console.log(count);
+
+// Part 2
+let count2 = 0;
+
+for (let i = 3; i < arr.length; i++) {
+  let currentRow = arr[i] + arr[i - 1] + arr[i - 2];
+  let lastRow = arr[i - 1] + arr[i - 2] + arr[i - 3];
+  if (currentRow > lastRow) {
+    count2++;
+  }
+}
+
+console.log(count2);
